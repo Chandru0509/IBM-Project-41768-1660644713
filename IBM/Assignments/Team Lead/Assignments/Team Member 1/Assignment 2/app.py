040fec46eef069e03bc6f3553eda958715a36922
Chandru0509 <96341905+Chandru0509@users.noreply.github.com>
2022-11-16 23:09:28 +0530

    Add files via upload

---
+++ IBM/Assignments/Team Lead/Assignments/Team Member 1/Assignment 2/app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template,redirect
+app = Flask(__name__)
+@app.route('/')
+def login():
+    return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+if __name__ == '__main__':
+    app.run(port=8080,debug=True)
